3778dbb189046a996a5cee04e561ea92ef59ae65
alisa <104276323+alisa-yar@users.noreply.github.com>
2023-03-20 01:07:09 +0200

    Update MyProject.java

--- MyProject.java
+++ MyProject.java
@@ -5,5 +5,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MyProject extends Application {
-  
-}
+  public void start(Stage stage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("MyProject.fxml"));
+    Parent root = loader.load();
+    Scene scene = new Scene(root); 
+    stage.setTitle("MyProject"); 
+    stage.setScene(scene); 
+    stage.show();
+   }
+   public static void main(String[] args) {
+    launch(args);
+    System.out.println();
+   }
+ }
+
