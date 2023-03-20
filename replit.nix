90b3f0516147339694c76004acee58244cc932b6
alisa <104276323+alisa-yar@users.noreply.github.com>
2023-03-20 20:07:13 +0200

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -6,6 +6,7 @@
         pkgs.replitPackages.java-debug
     ];
 }
+
 # replit.nix = Every new repl is now a Nix repl, which means you can install any package available on Nix.
 # The 'deps' array specifies which Nix packages you would like to be available in your environment.
 # The .replit file allows you to configure many options for your repl, most basic of which is the run command.
