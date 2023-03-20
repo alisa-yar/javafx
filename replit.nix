8f213ba1548b0b9f86c940ecb32ba60385a6b0e0
alisa <104276323+alisa-yar@users.noreply.github.com>
2023-03-20 20:05:44 +0200

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,8 +1,10 @@
 { pkgs }: {
     deps = [
-        pkgs.graalvm17-ce
+        pkgs.graalvm17-ce 
         pkgs.maven
         pkgs.replitPackages.jdt-language-server
         pkgs.replitPackages.java-debug
     ];
 }
+# replit.nix: 
+# Every new repl is now a Nix repl, which means you can install any package available on Nix
