bd847021aeda2e9752238ad7a8e2d0b83406364e
alisa <104276323+alisa-yar@users.noreply.github.com>
2023-03-22 00:00:53 +0200

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -7,7 +7,10 @@
 # and lower infrastructure costs on premises and in the cloud
 # pkgs.maven = Apache Maven is a software project management and comprehension tool. 
 # Based on the concept of a project object model (POM), Maven can manage a project's build, reporting and documentation from a central piece of information.
-
+# pkgs.jdt-language-server = Java language server. 
+# The Eclipse JDT Language Server is a Java language specific implementation of the Language Server Protocol 
+# and can be used with any editor that supports the protocol, to offer good support for the Java Language
+# https://github.com/eclipse/eclipse.jdt.ls
 { pkgs }: {
     deps = [
         pkgs.unzip
