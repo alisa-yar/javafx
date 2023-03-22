c07e7cafa707c05b07dddfcdb392a2612c9f82dc
alisa <104276323+alisa-yar@users.noreply.github.com>
2023-03-22 21:15:19 +0200

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -66,11 +66,17 @@
     };
 }
 
-# pkgs.lib.makeLibraryPath =
-# pkgs.xorg.libX11 = 
-# pkgs.xorg.libXxf86vm = X.Org Xxf86vm library
-# pkgs.libGL = 
-# pkgs.xorg.libXtst = X.Org Xlib-based client API for the XTEST & RECORD extensions library
+########## pkgs.lib.makeLibraryPath ##########
+
+########## pkgs.xorg.libX11 ########## 
+
+########## pkgs.xorg.libXxf86vm########## 
+# X.Org Xxf86vm library
+
+########## pkgs.libGL ########## 
+
+########## pkgs.xorg.libXtst ########## 
+# X.Org Xlib-based client API for the XTEST & RECORD extensions library
 
 
 
