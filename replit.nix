06f987d90c13788aa47a48418bdf0ac6740faaf1
alisa <104276323+alisa-yar@users.noreply.github.com>
2023-03-22 20:53:25 +0200

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -38,7 +38,6 @@
         pkgs.replitPackages.jdt-language-server
         pkgs.replitPackages.java-debug
         pkgs.xorg.libX11
-				# pkgs.scenebuilder
     ];
     env = {
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
